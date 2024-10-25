ce820d7309f968a9fb40a4ea01dea89a0e863630
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-24 21:53:51 -0500

    added https to render url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'job-board-frontend-h9y9.onrender.com'  # Frontend URL
+    origins 'http://localhost:5173', 'https://job-board-frontend-h9y9.onrender.com'  # Frontend URL
 
     resource '*',
       headers: :any,
